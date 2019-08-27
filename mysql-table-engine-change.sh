29ee65fa8beb6cd81d74c5a9e5632edc37866749
KwangILDaegu <pki054@naver.com>
2019-08-27 13:43:31 +0900

    1. 이전 엔진 옵션 제거 2. 명령어 실행 시, 버그 수정

--- mysql-table-engine-change.sh
+++ mysql-table-engine-change.sh
@@ -4,13 +4,12 @@ USER=""
 PASSWORD=""
 DATABASE=""
 
-PREV_ENGINE=$1;
 AFTER_ENGINE=$2;
 
-IFS=$'\n' ARRAY=("mysql -u$USER -p$PASSWORD -D$DATABASE -e 'show tables'")
+IFS=$'\n' ARRAY=(`mysql -u$USER -p$PASSWORD -D$DATABASE -e 'show tables' | grep -v 'Tables_in_$DATABASE'`)
 
 for VALUE in "${ARRAY[@]}"; do
-    mysql -u$USER -p$PASSWORD -D$DATABASE -e "alter table $PREV_ENGINE engine=$AFTER_ENGINE;";
+    mysql -u$USER -p$PASSWORD -D$DATABASE -e "alter table $VALUE engine=$AFTER_ENGINE;";
 
 	if [[ $? == 0 ]]; then
 		echo "<---- $VALUE Change Complete---->";
